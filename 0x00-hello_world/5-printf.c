f6983c2b9ed1aeeaf5baffe618a616adb33577a5
BILALSARBAH <bilalsarbah@gmail.com>
2023-02-16 20:42:53 -0800

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-        printf("with proper grammer, but the outcome is a piece os art,\n");
+        printf("\"with proper grammer, but the outcome is a piece os art,\n");
         return (0);
 
 }
