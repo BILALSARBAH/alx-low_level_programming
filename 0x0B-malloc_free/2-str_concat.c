bfd5057ed199c3619de47944f73826ddd24b0a6c
BILALSARBAH <bilalsarbah@gmail.com>
2023-03-14 09:18:19 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,6 +9,7 @@
  */
 
 char *str_concat(char *s1, char *s2)
+
 {
 	char *conct;
 	int i, ci;
@@ -18,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-		i = ci = 0;
+	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
@@ -29,18 +30,14 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	i = ci = 0;
 	while (s1[i] != '\0')
-
 	{
-
 		conct[i] = s1[i];
 		i++;
-
 	}
 
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
-
 		i++, ci++;
 	}
 	conct[i] = '\0';
